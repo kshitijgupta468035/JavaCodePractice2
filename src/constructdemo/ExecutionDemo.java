d94c90c952c4d0ff303b386277333f3069b37a54
kshitijgupta468035 <kshitij.gupta1_cs18@gla.ac.in>
2019-08-25 23:14:18 +0530

    add two output statements

--- src/constructdemo/ExecutionDemo.java
+++ src/constructdemo/ExecutionDemo.java
@@ -3,5 +3,7 @@ package constructdemo;
 public class ExecutionDemo {
     public static void main(String[] args) {
         ConstructDemo c1 = new ConstructDemo(45, "don");
+        System.out.println(c1.getNumber());
+        System.out.println(c1.getName());
     }
 }
